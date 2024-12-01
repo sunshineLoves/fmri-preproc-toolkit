66dcf65b12030e2dec534220e2e8d41ecc13ec1d
LeoJustinLab <2153409@tongji.edu.cn>
2024-12-02 00:17:06 +0800

    convert_fMRIvols_to_atlas.py 脚本中增加进度条，优化脑区提取过程，numpy 数组保存逻辑回退为 np.savetxt

--- convert/convert_fMRIvols_to_atlas.py
+++ convert/convert_fMRIvols_to_atlas.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import nibabel as nib
+from tqdm import tqdm
 import numpy as np
 import argparse
 
@@ -8,8 +9,9 @@ import argparse
 def convert_fMRIvols_to_atlas(fmri_pattern, output_path, atlas_file):
     # Where is the data located?
     fmri_files = glob.glob(fmri_pattern)
+    fmri_count = len(fmri_files)
     print("fMRI data pattern specified:", fmri_pattern)
-    print("Number of fMRI files:", len(fmri_files))
+    print("Number of fMRI files:", fmri_count)
     print("Atlas file:", atlas_file)
     try:
         label_img = nib.load(atlas_file)
@@ -21,15 +23,18 @@ def convert_fMRIvols_to_atlas(fmri_pattern, output_path, atlas_file):
             unique_labels.astype(np.int64), np.arange(nParcels + 1)
         ), f"Labels must be integers from 0 to {nParcels}"
         print("Atlas successfully loaded")
+        parcel_to_indices = {i: np.where(label == i + 1)[0] for i in range(0, nParcels)}
     except Exception as e:
         print(f"Loading dlabel Atlas File Error for {atlas_file}: {str(e)}")
 
     # Create output directory
     os.makedirs(output_path, exist_ok=True)
-    # Create fMRI .npy files
-    for fmri_file in fmri_files:
+    # Create fMRI .dat files
+    for index, fmri_file in enumerate(fmri_files):
         # Load images and labels
         if ".nii.gz" in fmri_file:
+            file_name = os.path.basename(fmri_file)
+            print(f"Processing {index + 1}/{fmri_count} {file_name}")
             print(f"Loading 4D image from {fmri_file}")
 
             try:
@@ -37,10 +42,10 @@ def convert_fMRIvols_to_atlas(fmri_pattern, output_path, atlas_file):
                 dts = dts_img.get_fdata()
                 print("Loaded fMRI data")
             except Exception as e:
-                print(f"Loading 4D File Error for {fmri_file}: {str(e)}")
+                print(f"Loading 4D File Error for {file_name}: {str(e)}")
 
             try:
-                print(f"Extracting Parcels for {fmri_file}")
+                print(f"Extracting Parcels for {file_name}")
                 m = dts.reshape((-1, dts.shape[-1])).T
                 sh = m.shape[0]
 
@@ -50,10 +55,9 @@ def convert_fMRIvols_to_atlas(fmri_pattern, output_path, atlas_file):
                 pMeas = np.zeros((nParcels, 3))
                 pmTS = np.zeros((sh, nParcels))
 
-                for i in range(1, nParcels + 1):
-                    ind = label == i
-                    y = m[:, ind]
-                    pmTS[:, i - 1] = np.nanmean(y, axis=1)
+                items = parcel_to_indices.items()
+                for i, indices in tqdm(items, desc="Extracting parcels", unit="parcel"):
+                    pmTS[:, i] = np.nanmean(m[:, indices], axis=1)
 
                 # Replace NaNs with 0
                 pmTS[np.isnan(pmTS)] = 0
@@ -62,18 +66,18 @@ def convert_fMRIvols_to_atlas(fmri_pattern, output_path, atlas_file):
                 extra_columns = np.repeat(last_column, 20, axis=0)
                 extended_pmTS = np.vstack((pmTS, extra_columns))
                 # Save Time Series
-                save_name = os.path.basename(fmri_file).split(".nii.gz")[0]
-                fn = os.path.join(output_path, f"{save_name}.npy")
+                save_name = file_name.split(".nii.gz")[0]
+                fn = os.path.join(output_path, f"{save_name}.dat")
                 print(
                     f"Saving file {fn} with shape {extended_pmTS.shape} (TRs, parcels)"
                 )
-                np.save(fn, extended_pmTS)
+                np.savetxt(fn, extended_pmTS)
 
             except:
-                print(f"Error with parcel Extraction for {fmri_file}")
+                print(f"Error with parcel Extraction for {file_name}")
 
         else:
-            print(f"File {fmri_file} not a nifti file. Skipping...")
+            print(f"File {file_name} not a nifti file. Skipping...")
 
 
 def main():
